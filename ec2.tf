bda000f51dfb284a03f719e2deaf9bb0615e544b
okimurak <okimurak0901@gmail.com>
2019-12-08 18:25:30 +0900

    Add iam and s3

--- ec2.tf
+++ ec2.tf
@@ -3,3 +3,27 @@ module "sandbox" {
   name     = "Test EC2"
   security_group_name = "Test EC2 Security Group"
 }
+
+module "dev_server" {
+  source = "./modules/httpserver"
+  instance_type = "t3.micro"
+}
+
+ data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect = "Allow"
+    actions = ["ec2:DescribeRegions"] # リージョン⼀覧を取得する
+    resources = ["*"]
+  }
+}
+
+module "describe_regions_for_ec2" {
+  source = "./modules/iam_role"
+  name = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
+output "public_dns" {
+  value = module.dev_server.public_dns
+}
--- modules/s3/s3_bucket.tf
+++ modules/s3/s3_bucket.tf
@@ -3,4 +3,20 @@ resource "aws_s3_bucket" "bucket" {
   versioning {
     enabled = var.is_versioning
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket = aws_s3_bucket.bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
